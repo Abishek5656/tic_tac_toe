1388af495a88e35ed754a62d0acd18d227201bcf
abishekvarmapenmetsa <abishekvarma@newowl.co>
2025-11-21 17:53:19 +0530

    updated the nakama config files in frontend

--- frontend/src/nakama/nakama.config.js
+++ frontend/src/nakama/nakama.config.js
@@ -1,9 +1,6 @@
-// src/nakama/nakama.config.js
-const nakamaConfig = {
+export default {
   host: import.meta.env.VITE_NAKAMA_HOST || "localhost",
   port: Number(import.meta.env.VITE_NAKAMA_PORT) || 7350,
   serverKey: import.meta.env.VITE_NAKAMA_SERVER_KEY || "defaultkey",
-  useSSL: import.meta.env.VITE_NAKAMA_USE_SSL === "true",
+  useSSL: import.meta.env.VITE_NAKAMA_USE_SSL === "true" ? true : false,
 };
-
-export default nakamaConfig;
--- frontend/src/nakama/nakamaClient.js
+++ frontend/src/nakama/nakamaClient.js
@@ -1,17 +1,41 @@
-// src/nakama/nakamaClient.js
+
 import { Client } from "@heroiclabs/nakama-js";
 import config from "./nakama.config";
 
-let client;
+let client = null;
 
+/** Create or return singleton client */
 export const getNakamaClient = () => {
   if (!client) {
-    client = new Client(
-      config.useSSL ? "https" : "http",
-      config.host,
-      config.port,
-      config.serverKey
-    );
+    // client = new Client(
+    //   config.serverKey,      // defaultkey
+    //   config.host,          // localhost
+    //   config.port,          // 7350
+    //   config.useSSL         // false → USE HTTP
+    // );
+ client =  new Client(
+  config.serverKey,
+  config.host,
+  config.port,
+  config.useSSL
+);
   }
   return client;
+};
+
+/** Authenticate user with custom ID (username) */
+export const authenticateUser = async (username) => {
+  const client = getNakamaClient();
+
+    console.log("###username", username)
+
+  try {
+    const session = await client.authenticateCustom(username, true);
+    console.log("username", username)
+    console.log("Nakama Authenticated:", session);
+    return session;
+  } catch (err) {
+    console.error("Authentication failed:", err);
+    throw err;
+  }
 };
